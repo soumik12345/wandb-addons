6c0924945e6b292e3ff47f74cadcf8c43e490ba5
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2023-03-20 16:23:35 +0000

    update: linting

--- wandb_addons/monai/stats_handler.py
+++ wandb_addons/monai/stats_handler.py
@@ -6,11 +6,14 @@ if TYPE_CHECKING:
     from ignite.engine import Engine
 else:
     Engine, _ = optional_import(
-        "ignite.engine", IgniteInfo.OPT_IMPORT_VERSION, min_version, "Engine", as_type="decorator"
+        "ignite.engine",
+        IgniteInfo.OPT_IMPORT_VERSION,
+        min_version,
+        "Engine",
+        as_type="decorator",
     )
 
 
 class WandbStatsHandler:
-
     def __init__(self):
         pass
