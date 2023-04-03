37e35dd530e51bef63eb63b1c85eb55ed8b92334
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2023-04-03 19:45:52 +0000

    update: docstring for MonAI handlers

--- wandb_addons/monai/checkpoint_handler.py
+++ wandb_addons/monai/checkpoint_handler.py
@@ -37,54 +37,55 @@ class WandbModelCheckpointHandler(ModelCheckpoint):
         save_on_rank: int = 0,
         **kwargs: Any,
     ):
-        """Args:
-        dirname: Union[str, Path].
-            Directory path where objects will be saved.
-        filename_prefix: str.
-            Prefix for the file names to which objects will be saved. See Notes of
-            :class:`~ignite.handlers.checkpoint.Checkpoint` for more details.
-        save_interval: Optional[int].
-            If not None, it should be a function taking a single argument, an
-            :class:`~ignite.engine.engine.Engine` object, and return a score (`float`). Objects
-            with highest scores will be retained.
-        score_name: Optional[Callable].
-            If `score_function` not None, it is possible to store its value using `score_name`.
-            See Examples of :class:`~ignite.handlers.checkpoint.Checkpoint` for more details.
-        n_saved: Union[int, None].
-            Number of objects that should be kept on disk. Older files will be removed. If set to
-            `None`, all objects are kept.
-        atomic: bool.
-            If True, objects are serialized to a temporary file, and then moved to final
-            destination, so that files are guaranteed to not be damaged (for example if exception
-            occurs during saving).
-        require_empty: bool.
-            If True, will raise exception if there are any files starting with `filename_prefix`
-            in the directory `dirname`.
-        create_dir: bool.
-            If True, will create directory `dirname` if it does not exist.
-        global_step_transform: Optional[Callable].
-            Global step transform function to output a desired global step. Input of the function
-            is `(engine, event_name)`. Output of function should be an integer. Default is None,
-            `global_step` based on attached engine. If provided, uses function output as
-            `global_step`. To setup global step from another engine, please use
-            :meth:`~ignite.handlers.global_step_from_engine`.
-        archived: bool.
-            Deprecated argument as models saved by `torch.save` are already compressed.
-        filename_pattern: Optional[str].
-            If `filename_pattern` is provided, this pattern will be used to render checkpoint
-            filenames. If the pattern is not defined, the default pattern would be used. See
-            :class:`~ignite.handlers.checkpoint.Checkpoint` for details.
-        include_self: bool.
-            Whether to include the `state_dict` of this object in the checkpoint. If `True`, then
-            there must not be another object in `to_save` with key `checkpointer`.
-        greater_or_equal: bool.
-            If `True`, the latest equally scored model is stored. Otherwise, the first model.
-            Default, `False`.
-        save_on_rank: int.
-            Which rank to save the objects on, in the distributed configuration. Used to instantiate
-            a :class:`~ignite.handlers.DiskSaver` and is also passed to the parent class.
-        kwargs: Any.
-            Accepted keyword arguments for `torch.save` or `xm.save` in `DiskSaver`.
+        """
+        # Arguments:
+            dirname: Union[str, Path].
+                Directory path where objects will be saved.
+            filename_prefix: str.
+                Prefix for the file names to which objects will be saved. See Notes of
+                :class:`~ignite.handlers.checkpoint.Checkpoint` for more details.
+            save_interval: Optional[int].
+                If not None, it should be a function taking a single argument, an
+                :class:`~ignite.engine.engine.Engine` object, and return a score (`float`). Objects
+                with highest scores will be retained.
+            score_name: Optional[Callable].
+                If `score_function` not None, it is possible to store its value using `score_name`.
+                See Examples of :class:`~ignite.handlers.checkpoint.Checkpoint` for more details.
+            n_saved: Union[int, None].
+                Number of objects that should be kept on disk. Older files will be removed. If set to
+                `None`, all objects are kept.
+            atomic: bool.
+                If True, objects are serialized to a temporary file, and then moved to final
+                destination, so that files are guaranteed to not be damaged (for example if exception
+                occurs during saving).
+            require_empty: bool.
+                If True, will raise exception if there are any files starting with `filename_prefix`
+                in the directory `dirname`.
+            create_dir: bool.
+                If True, will create directory `dirname` if it does not exist.
+            global_step_transform: Optional[Callable].
+                Global step transform function to output a desired global step. Input of the function
+                is `(engine, event_name)`. Output of function should be an integer. Default is None,
+                `global_step` based on attached engine. If provided, uses function output as
+                `global_step`. To setup global step from another engine, please use
+                :meth:`~ignite.handlers.global_step_from_engine`.
+            archived: bool.
+                Deprecated argument as models saved by `torch.save` are already compressed.
+            filename_pattern: Optional[str].
+                If `filename_pattern` is provided, this pattern will be used to render checkpoint
+                filenames. If the pattern is not defined, the default pattern would be used. See
+                :class:`~ignite.handlers.checkpoint.Checkpoint` for details.
+            include_self: bool.
+                Whether to include the `state_dict` of this object in the checkpoint. If `True`, then
+                there must not be another object in `to_save` with key `checkpointer`.
+            greater_or_equal: bool.
+                If `True`, the latest equally scored model is stored. Otherwise, the first model.
+                Default, `False`.
+            save_on_rank: int.
+                Which rank to save the objects on, in the distributed configuration. Used to instantiate
+                a :class:`~ignite.handlers.DiskSaver` and is also passed to the parent class.
+            kwargs: Any.
+                Accepted keyword arguments for `torch.save` or `xm.save` in `DiskSaver`.
         """
         if wandb.run is None:
             raise wandb.Error(
