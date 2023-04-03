81523ceb342093a9d7c1aa53d42f71d2bfa9fb4b
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2023-04-03 17:58:40 +0000

    update: refactor docstrings to Google Python Style

--- wandb_addons/utils.py
+++ wandb_addons/utils.py
@@ -6,16 +6,19 @@ def fetch_wandb_artifact(artifact_address: str, artifact_type: str) -> FilePathS
     """
     Utility function for fetching a [Weights & Biases artifact](https://docs.wandb.ai/guides/artifacts)
     irrespective of whether a [run](https://docs.wandb.ai/guides/runs) has been initialized or not.
-
-    Args:
-        artifact_address (str): A human-readable name for the artifact, which is how you can identify
-            this artifact in the UI or reference it in use_artifact calls. Names can contain letters,
-            numbers, underscores, hyphens, and dots. The name must be unique across a project.
-        artifact_type: The type of the artifact, which is used to organize and differentiate artifacts.
-            Common types include dataset or model, but you can use any string containing letters, numbers,
-            underscores, hyphens, and dots.
-
-    Returns:
+    
+    # Arguments:
+        artifact_address: str.
+            A human-readable name for the artifact, which is how you can identify the artifact in the UI
+            or reference it in [`use_artifact`](https://docs.wandb.ai/ref/python/run#use_artifact) calls.
+            Names can contain letters, numbers, underscores, hyphens, and dots. The name must be unique
+            across a project.
+        artifact_type: str.
+            The type of the artifact, which is used to organize and differentiate artifacts. Common types
+            include dataset or model, but you can use any string containing letters, numbers, underscores,
+            hyphens, and dots.
+    
+    # Returns:
         (wandb.util.FilePathStr): The path to the downloaded contents.
     """
     return (
