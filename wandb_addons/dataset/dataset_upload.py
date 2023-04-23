a1f71a2a515801060528a0a610a86b20224a510f
soumik12345 <19soumik.rakshit96@gmail.com>
2023-04-23 12:23:06 +0000

    update: upload functionality to upload_dataset()

--- wandb_addons/dataset/dataset_upload.py
+++ wandb_addons/dataset/dataset_upload.py
@@ -8,6 +8,7 @@ import wandb
 import tensorflow_datasets as tfds
 
 from .utils import _create_empty_file
+from ..utils import upload_wandb_artifact
 
 
 def _upload_with_builder_script(name: str, path: str) -> Union[bool, None]:
@@ -15,6 +16,7 @@ def _upload_with_builder_script(name: str, path: str) -> Union[bool, None]:
 
     if not os.path.isfile(builder_script_path):
         raise wandb.Error(f"Unable to locate builder script {builder_script_path}")
+    wandb.termlog(f"Located builder script {builder_script_path}")
 
     builder_script_module_path = os.path.join(path, name)
     os.makedirs(builder_script_module_path)
@@ -24,7 +26,6 @@ def _upload_with_builder_script(name: str, path: str) -> Union[bool, None]:
     updated_builder_script_file = (
         builder_script_path.split("/")[-1].split(".")[0] + "_dataset_builder.py"
     )
-    wandb.termlog(updated_builder_script_file)
     shutil.move(
         builder_script_path,
         os.path.join(builder_script_module_path, updated_builder_script_file),
@@ -32,7 +33,6 @@ def _upload_with_builder_script(name: str, path: str) -> Union[bool, None]:
 
     current_working_dir = os.getcwd()
     os.chdir(builder_script_module_path)
-    wandb.termlog(os.getcwd())
     result = subprocess.run(shlex.split("tfds build"))
     os.chdir(current_working_dir)
 
@@ -50,8 +50,35 @@ def _upload_with_builder_script(name: str, path: str) -> Union[bool, None]:
         _create_empty_file(os.path.join(path, "__init__.py"))
 
 
-def upload_dataset(name: str, path: str):
+def _upload_with_tfds_directory(name: str, path: str):
+    builder_module_path = os.path.join(path, name)
+
+    if not os.path.isdir(builder_module_path):
+        raise wandb.Error(f"Unable to locate tfds module {builder_module_path}")
+
+    current_working_dir = os.getcwd()
+    os.chdir(builder_module_path)
+    result = subprocess.run(shlex.split("tfds build"))
+    os.chdir(current_working_dir)
+
+    if result.returncode != 0:
+        wandb.termerror("Unable to build Tensorflow Dataset")
+        subprocess.run(shlex.split("rm -rf ~/tensorflow_datasets/"))
+        raise wandb.Error("Unable to build Tensorflow Dataset")
+
+    if not os.path.isfile(os.path.join(path, "__init__.py")):
+        _create_empty_file(os.path.join(path, "__init__.py"))
+
+
+def upload_dataset(name: str, path: str, type: str):
     try:
-        _upload_with_builder_script(name, path)
+        _upload_with_tfds_directory(name, path)
+        upload_wandb_artifact(name, type, path)
     except wandb.Error as e:
-        wandb.termerror(e)
+        wandb.termwarn(str(e))
+        wandb.termlog("Attempting to locate builder script...")
+        try:
+            _upload_with_builder_script(name, path)
+            upload_wandb_artifact(name, type, path)
+        except wandb.Error as e:
+            wandb.termerror(e)
