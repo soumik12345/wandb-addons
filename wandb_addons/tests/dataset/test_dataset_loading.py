46ec959b3166a05dc5eaa368e8ee1bd4a0687b7f
soumik12345 <19soumik.rakshit96@gmail.com>
2023-04-23 13:55:46 +0000

    add: unit tests for dataset loading

---
+++ wandb_addons/tests/dataset/test_dataset_loading.py
@@ -0,0 +1,27 @@
+import unittest
+
+from wandb_addons.dataset import load_dataset
+
+
+class DatasetLoadingTestCase(unittest.TestCase):
+    def __init__(self, methodName: str = "runTest") -> None:
+        super().__init__(methodName)
+        self.datasets, self.dataset_builder_info = load_dataset(
+            "geekyrakshit/monkey-dataset/monkey_species:v2", quiet=True
+        )
+
+    def test_classes(self) -> None:
+        class_names = self.dataset_builder_info.features["label"].names
+        self.assertEqual(len(class_names), 10)
+
+    def test_train_split(self) -> None:
+        self.assertEqual(
+            tuple(self.datasets["train"].element_spec["image"].shape), (None, None, 3)
+        )
+        self.assertEqual(tuple(self.datasets["train"].element_spec["label"].shape), ())
+
+    def test_val_split(self) -> None:
+        self.assertEqual(
+            tuple(self.datasets["val"].element_spec["image"].shape), (None, None, 3)
+        )
+        self.assertEqual(tuple(self.datasets["val"].element_spec["label"].shape), ())
