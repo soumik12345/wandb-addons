197f6aa0308db10c5a44a7e6692df62ee5ec1c54
soumik12345 <19soumik.rakshit96@gmail.com>
2023-04-24 20:37:43 +0000

    add: functionality to automatically load datasets from artifacts contaning tfrecords

--- wandb_addons/dataset/dataset_loading.py
+++ wandb_addons/dataset/dataset_loading.py
@@ -22,6 +22,50 @@ from .utils import (
 _DATASET_TYPE = Any
 
 
+def _load_dataset_from_tfds_module(
+    artifact_dir: str,
+    dataset_name: str,
+    remove_redundant_data_files: bool = True,
+    quiet: bool = False,
+) -> Tuple[Dict[str, _DATASET_TYPE], DatasetInfo]:
+    wandb.termlog("Creating __init__.py inside `artifact_dir`...")
+    _create_empty_file(os.path.join(artifact_dir, "__init__.py"))
+    wandb.termlog("Done!")
+
+    # build and prepare the dataset to `~/tensorflow_datasets/
+    wandb.termlog(f"Building dataset {dataset_name}...")
+    current_working_dir = os.getcwd()
+    os.chdir(os.path.join(artifact_dir, dataset_name))
+    if not quiet:
+        subprocess.call(shlex.split("tfds build"))
+    else:
+        tf.get_logger().setLevel("ERROR")
+        subprocess.call(
+            shlex.split("tfds build"),
+            stderr=subprocess.DEVNULL,
+            stdout=subprocess.DEVNULL,
+        )
+    os.chdir(current_working_dir)
+    wandb.termlog(f"Built dataset {dataset_name}!")
+
+    if remove_redundant_data_files:
+        wandb.termlog(f"Removing redundant files from {artifact_dir}...")
+        _remove_redundant_files(artifact_dir, dataset_name)
+        wandb.termlog("Done!")
+
+    try:
+        wandb.termlog(f"Registering dataset {dataset_name}...")
+        exec(_get_dataset_registration_statement(artifact_dir, dataset_name))
+        wandb.termlog(f"Registered dataset {dataset_name}!")
+    except ImportError as exception:
+        print(exception)
+        raise wandb.Error(f"Unable to register {artifact_dir}.{dataset_name}")
+
+    dataset_builder = tfds.builder("monkey_species")
+    dataset_builder.download_and_prepare()
+    dataset_splits, dataset_builder_info = _build_datasets(dataset_builder)
+
+
 def load_dataset(
     artifact_address: str,
     artifact_type: Optional[str] = "dataset",
@@ -73,44 +117,18 @@ def load_dataset(
         artifact_address=artifact_address, artifact_type=artifact_type
     )
     artifact_dir = _change_artifact_dir_name(artifact_dir)
-
-    wandb.termlog("Creating __init__.py inside `artifact_dir`...")
-    _create_empty_file(os.path.join(artifact_dir, "__init__.py"))
-    wandb.termlog("Done!")
-
     dataset_name = _get_dataset_name_from_artifact_address(artifact_address)
 
-    # build and prepare the dataset to `~/tensorflow_datasets/
-    wandb.termlog(f"Building dataset {dataset_name}...")
-    current_working_dir = os.getcwd()
-    os.chdir(os.path.join(artifact_dir, dataset_name))
-    if not quiet:
-        subprocess.call(shlex.split("tfds build"))
-    else:
-        tf.get_logger().setLevel("ERROR")
-        subprocess.call(
-            shlex.split("tfds build"),
-            stderr=subprocess.DEVNULL,
-            stdout=subprocess.DEVNULL,
-        )
-    os.chdir(current_working_dir)
-    wandb.termlog(f"Built dataset {dataset_name}!")
-
-    if remove_redundant_data_files:
-        wandb.termlog(f"Removing redundant files from {artifact_dir}...")
-        _remove_redundant_files(artifact_dir, dataset_name)
-        wandb.termlog("Done!")
-
     try:
-        wandb.termlog(f"Registering dataset {dataset_name}...")
-        exec(_get_dataset_registration_statement(artifact_dir, dataset_name))
-        wandb.termlog(f"Registered dataset {dataset_name}!")
-    except ImportError as exception:
-        print(exception)
-        raise wandb.Error(f"Unable to register {artifact_dir}.{dataset_name}")
-
-    dataset_builder = tfds.builder("monkey_species")
-    dataset_builder.download_and_prepare()
-    dataset_splits, dataset_builder_info = _build_datasets(dataset_builder)
+        dataset_builder = tfds.builder_from_directory(artifact_dir)
+        dataset_builder.download_and_prepare()
+        dataset_splits, dataset_builder_info = _build_datasets(dataset_builder)
+    except Exception as e:
+        wandb.termwarn(
+            "Failed to detect TFRecords in the artifact. Attempting to build tfrecords"
+        )
+        dataset_splits, dataset_builder_info = _load_dataset_from_tfds_module(
+            artifact_dir, dataset_name, remove_redundant_data_files, quiet
+        )
 
     return dataset_splits, dataset_builder_info
