83309a14fe80741a68c7881967fb7e5fa7dc1e7a
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2023-08-16 18:50:28 +0000

    add: docstring for WandBDetectionVisualizationCallback

--- wandb_addons/keras/detection/callback.py
+++ wandb_addons/keras/detection/callback.py
@@ -11,11 +11,38 @@ from .inference import get_mean_confidence_per_class
 
 
 class WandBDetectionVisualizationCallback(keras.callbacks.Callback):
+    """Callback for visualizing ground-truth and predicted bounding boxes in an
+    epoch-wise manner for an object-detection task for
+    [KerasCV](https://github.com/keras-team/keras-cv). The callback logs a
+    [`wandb.Table`](https://docs.wandb.ai/guides/tables) with columns for the epoch,
+    the images overlayed with an interactive bounding box overlay corresponding to the
+    ground-truth and predicted boudning boxes, the number of ground-truth bounding
+    boxes and the predicted mean-confidence for each class.
+
+    !!! example "Example notebooks:"
+        - [Fine-tuning an Object Detection Model using KerasCV](../examples/visualize_dataset).
+
+    Arguments:
+        dataset (tf.data.Dataset): A batched dataset consisting of Ragged Tensors.
+            This can be obtained by applying `ragged_batch()` on a `tf.data.Dataset`.
+        class_mapping (Dict[int, str]): A dictionary that maps the index of the classes
+            to the corresponding class names.
+        max_batches_to_visualize (Optional[int]): Maximum number of batches from the
+            dataset to be visualized.
+        iou_threshold (float): IoU threshold for non-max suppression during prediction.
+        confidence_threshold (float): Confidence threshold for non-max suppression
+            during prediction.
+        source_bbox_format (str): Format of the source bounding box, one of `"xyxy"`
+            or `"xywh"`.
+        title (str): Title under which the table will be logged to the Weights & Biases
+            workspace.
+    """
+
     def __init__(
         self,
         dataset: tf_data.Dataset,
         class_mapping: dict,
-        max_batches_for_vis: Optional[Union[int, None]] = 1,
+        max_batches_to_visualize: Optional[Union[int, None]] = 1,
         iou_threshold: float = 0.01,
         confidence_threshold: float = 0.01,
         source_bounding_box_format: str = "xywh",
@@ -24,9 +51,9 @@ class WandBDetectionVisualizationCallback(keras.callbacks.Callback):
         **kwargs
     ):
         super().__init__(*args, **kwargs)
-        self.dataset = dataset.take(max_batches_for_vis)
+        self.dataset = dataset.take(max_batches_to_visualize)
         self.class_mapping = class_mapping
-        self.max_batches_for_vis = max_batches_for_vis
+        self.max_batches_to_visualize = max_batches_to_visualize
         self.iou_threshold = iou_threshold
         self.confidence_threshold = confidence_threshold
         self.source_bounding_box_format = source_bounding_box_format
@@ -37,7 +64,14 @@ class WandBDetectionVisualizationCallback(keras.callbacks.Callback):
             iou_threshold=self.iou_threshold,
             confidence_threshold=self.confidence_threshold,
         )
-        self.table = wandb.Table(columns=["Epoch", "Image", "Mean-Confidence"])
+        self.table = wandb.Table(
+            columns=[
+                "Epoch",
+                "Image",
+                "Number-of-Ground-Truth-Boxes",
+                "Mean-Confidence",
+            ]
+        )
 
     def plot_prediction(self, epoch, image_batch, y_true_batch):
         y_pred_batch = self.model.predict(image_batch)
@@ -130,12 +164,14 @@ class WandBDetectionVisualizationCallback(keras.callbacks.Callback):
             mean_confidence_dict = get_mean_confidence_per_class(
                 confidences, predicted_classes, self.class_mapping
             )
-            self.table.add_data(epoch, wandb_image, mean_confidence_dict)
+            self.table.add_data(
+                epoch, wandb_image, len(gt_classes), mean_confidence_dict
+            )
 
     def on_epoch_end(self, epoch, logs):
         original_prediction_decoder = self.model._prediction_decoder
         self.model.prediction_decoder = self.prediction_decoder
-        for _ in tqdm(range(self.max_batches_for_vis)):
+        for _ in tqdm(range(self.max_batches_to_visualize)):
             image_batch, y_true_batch = next(iter(self.dataset))
             self.plot_prediction(epoch, image_batch, y_true_batch)
         self.model.prediction_decoder = original_prediction_decoder
