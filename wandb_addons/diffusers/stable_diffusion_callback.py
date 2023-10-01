89de45cf01181e251e6a98658d7e68ed9c337bc7
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2023-10-01 08:57:02 +0000

    update: docstring for WandBStableDiffusionCallback

--- wandb_addons/diffusers/stable_diffusion_callback.py
+++ wandb_addons/diffusers/stable_diffusion_callback.py
@@ -1,4 +1,4 @@
-from typing import Dict, Union, List, Optional
+from typing import Dict, List, Optional, Union
 
 import wandb
 from diffusers import StableDiffusionPipeline
@@ -7,15 +7,19 @@ from .utils import chunkify
 
 
 class WandBStableDiffusionCallback:
-    """Callback for [🧨 Diffusers](https://huggingface.co/docs/diffusers/index) logging the results of a
+    """Callback for [🧨 Diffusers](https://huggingface.co/docs/diffusers/index) logging
+    the results of a
     [`StableDiffusionPipeline`](https://huggingface.co/docs/diffusers/v0.9.0/en/api/pipelines/stable_diffusion#diffusers.StableDiffusionPipeline)
     generation to Weights & Biases.
 
     !!! note "Features:"
-        - The callback automatically logs basic configs like prompt, negative prompt, etc. along with the
-            generated image in a [`wandb.Table`](https://docs.wandb.ai/guides/tables).
-        - The callback also logs configs for both the experiment as well as pipelines with the wandb run.
-        - No need to initialize a run, the callback automatically initialized and ends runs gracefully.
+        - The callback automatically logs basic configs like prompt, negative prompt,
+            etc. along with the generated image in a
+            [`wandb.Table`](https://docs.wandb.ai/guides/tables).
+        - The callback also logs configs for both the experiment as well as pipelines
+            with the wandb run.
+        - No need to initialize a run, the callback automatically initialized and ends
+            runs gracefully.
 
     !!! example "Example usage:"
         You can fine an example notebook [here](../examples/stable_diffusion).
@@ -32,7 +36,10 @@ class WandBStableDiffusionCallback:
         )
         pipe = pipe.to("cuda")
 
-        prompt = ["a photograph of an astronaut riding a horse", "a photograph of a dragon"]
+        prompt = [
+            "a photograph of an astronaut riding a horse",
+            "a photograph of a dragon",
+        ]
 
         # Create the WandB callback for StableDiffusionPipeline
         callback = WandBStableDiffusionCallback(
@@ -44,30 +51,36 @@ class WandBStableDiffusionCallback:
         ```
 
     Arguments:
-        pipe (diffusers.StableDiffusionPipeline): The `StableDiffusionPipeline` from `diffusers`.
-        prompt (Union[str, List[str]]): The prompt or prompts to guide the image generation.
-        wandb_project (Optional[str]): The name of the project where you're sending the new run.
-            The project is not necessary to be specified unless the run has automatically been
-            initiatlized before the callback is defined.
-        wandb_entity (Optional[str]): An entity is a username or team name where you're sending runs.
-            This entity must exist before you can send runs there, so make sure to create your account
-            or team in the UI before starting to log runs. If you don't specify an entity, the run
-            will be sent to your default entity, which is usually your username. Change your default
-            entity in [your settings](https://wandb.ai/settings) under "default location to create
-            new projects".
-        num_inference_steps (int): The number of denoising steps. More denoising steps usually lead to
-            a higher quality image at the expense of slower inference.
-        num_images_per_prompt (Optional[int]): The number of images to generate per prompt.
+        pipe (diffusers.StableDiffusionPipeline): The `StableDiffusionPipeline` from
+            `diffusers`.
+        prompt (Union[str, List[str]]): The prompt or prompts to guide the image
+            generation.
+        wandb_project (Optional[str]): The name of the project where you're sending
+            the new run. The project is not necessary to be specified unless the run
+            has automatically been initiatlized before the callback is defined.
+        wandb_entity (Optional[str]): An entity is a username or team name where
+            you're sending runs. This entity must exist before you can send runs there,
+            so make sure to create your account or team in the UI before starting to
+            log runs. If you don't specify an entity, the run will be sent to your
+            default entity, which is usually your username. Change your default entity
+            in [your settings](https://wandb.ai/settings) under "default location to
+            create new projects".
+        num_inference_steps (int): The number of denoising steps. More denoising steps
+            usually lead to a higher quality image at the expense of slower inference.
+        num_images_per_prompt (Optional[int]): The number of images to generate per
+            prompt.
         guidance_scale (float): Guidance scale as defined in
-            [Classifier-Free Diffusion Guidance](https://arxiv.org/abs/2207.12598). `guidance_scale` is
-            defined as `w` of equation 2. of [Imagen Paper](https://arxiv.org/pdf/2205.11487.pdf).
-            Guidance scale is enabled by setting `guidance_scale > 1`. Higher guidance scale encourages
-            to generate images that are closely linked to the text `prompt`, usually at the expense of
-            lower image quality.
-        negative_prompt (Optional[Union[str, List[str]]]): The prompt or prompts not to guide the image
-            generation. Ignored when not using guidance (i.e., ignored if `guidance_scale` is less than `1`).
-        configs (Optional[Dict]): Additional configs for the experiment you want to sync, for example, seed
-            could be a good config to be passed here.
+            [Classifier-Free Diffusion Guidance](https://arxiv.org/abs/2207.12598).
+            `guidance_scale` is defined as `w` of equation 2. of
+            [Imagen Paper](https://arxiv.org/pdf/2205.11487.pdf). Guidance scale is
+            enabled by setting `guidance_scale > 1`. Higher guidance scale encourages
+            to generate images that are closely linked to the text `prompt`, usually
+            at the expense of lower image quality.
+        negative_prompt (Optional[Union[str, List[str]]]): The prompt or prompts not
+            to guide the image generation. Ignored when not using guidance
+            (i.e., ignored if `guidance_scale` is less than `1`).
+        configs (Optional[Dict]): Additional configs for the experiment you want to
+            sync, for example, seed could be a good config to be passed here.
     """
 
     def __init__(
