d42fd9adfde54ac3649146f89cf66160fd454470
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2023-10-10 00:10:31 +0000

    add: IFCallback

---
+++ wandb_addons/diffusers/callbacks/deepfloyd_if.py
@@ -0,0 +1,43 @@
+from typing import Dict, List, Optional, Union
+
+import torch
+from diffusers import DiffusionPipeline
+
+from .base import BaseDiffusersCallback
+
+
+class IFCallback(BaseDiffusersCallback):
+    def __init__(
+        self,
+        pipeline: DiffusionPipeline,
+        prompt: Union[str, List[str]],
+        wandb_project: str,
+        wandb_entity: Optional[str] = None,
+        weave_mode: bool = False,
+        num_inference_steps: int = 50,
+        num_images_per_prompt: Optional[int] = 1,
+        negative_prompt: Optional[Union[str, List[str]]] = None,
+        configs: Optional[Dict] = None,
+        **kwargs
+    ) -> None:
+        super().__init__(
+            pipeline,
+            prompt,
+            wandb_project,
+            wandb_entity,
+            weave_mode,
+            num_inference_steps,
+            num_images_per_prompt,
+            negative_prompt,
+            configs,
+            **kwargs
+        )
+
+    def generate(self, latents: torch.FloatTensor) -> List:
+        images = (latents / 2 + 0.5).clamp(0, 1)
+        images = images.cpu().permute(0, 2, 3, 1).float().numpy()
+        images, nsfw_detected, watermark_detected = self.pipeline.run_safety_checker(
+            images, self.pipeline._execution_device, latents.dtype
+        )
+        images = self.pipeline.numpy_to_pil(images)
+        return images
