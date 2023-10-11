10375f598a7d7ff3d5f98309b8b3e1371c4d590b
soumik12345 <19soumik.rakshit96@gmail.com>
2023-10-11 09:00:25 +0000

    update: refactor stable_diffusion module

---
+++ wandb_addons/diffusers/callbacks/stable_diffusion/__init__.py
@@ -0,0 +1,5 @@
+from .text_to_image import StableDiffusionCallback
+from .image_to_image import StableDiffusionImg2ImgCallback
+
+
+__all__ = ["StableDiffusionCallback", "StableDiffusionImg2ImgCallback"]
