f284ac81e76ad79bb96a10f5afde7ad7de7786ea
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2023-12-05 14:58:25 +0000

    update: docs

---
+++ docs/datatypes.md
@@ -0,0 +1,5 @@
+# Experimental Datatypes
+
+A list of experimental multi-modal datatypes that can be passed to a WandB loggable dictionary.
+
+::: wandb_addons.datatype.video
--- wandb_addons/datatype/video.py
+++ wandb_addons/datatype/video.py
@@ -32,6 +32,44 @@ def create_video_from_np_arrays(frames: List[np.array], fps: int) -> str:
 def loggable_video(
     video: Union[str, List[np.array]], video_format: str = "mp4", fps: int = 30
 ) -> Union[wandb.Html, None]:
+    """Function that logs a playable video with controls to contrast the default
+    uncontrollable gif offered by [`wandb.Video`](https://docs.wandb.ai/ref/python/data-types/video).
+
+    !!! example "Example WandB Run"
+        [https://wandb.ai/geekyrakshit/test/runs/q0ni305v](https://wandb.ai/geekyrakshit/test/runs/q0ni305v)
+
+    !!! example "Logging a video file"
+        ```python
+        import wandb
+        from wandb_addons.datatype import loggable_video
+
+        with wandb.init(project="test", entity="geekyrakshit"):
+            wandb.log({"Test-Video": "video.mp4"})
+        ```
+
+    !!! example "Logging a list of numpy arrays corresponding to frames"
+        ```python
+        import numpy as np
+
+        import wandb
+        from wandb_addons.datatype import loggable_video
+
+        with wandb.init(project="test", entity="geekyrakshit"):
+            frames = [np.ones((256, 256, 3)) * 255] * 10 + [np.zeros((256, 256, 3))] * 10
+            wandb.log({"Test-Video": loggable_video(frames)})
+        ```
+
+    Arguments:
+        video (Union[str, List[np.array]]): The path to a video file or a list of
+            numpy arrays of shape `(H, W, C)` corresponding to the frames of the video.
+        video_format (str): Format of the video.
+        fps (int): Frame-rate of the video, applicable only when logging list of
+            numpy arrays.
+
+    Returns:
+        (Union[wandb.Html, None]): A `wandb.Html` object that can be passed to a WandB
+            loggable dictionary.
+    """
     if isinstance(video, str):
         if os.path.isfile(video):
             with open(video, "rb") as video_file:
